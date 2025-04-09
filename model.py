21dcb77c3f3ba18d3813283466aa2d9f9e15e080
s298157 <s298157@studenti.polito.it>
2025-04-09 10:05:45 +0200

    Update

--- model.py
+++ model.py
@@ -2,68 +2,39 @@ import random
 
 class Model(object):
     def __init__(self):
-        self._NMax = 100
-        self._TMax = 6
-        self._T = self._TMax
-        self._segreto = None
+        self._numero_massimo = 100
+        self._tentativi_massimi = 6
+        self._tentativi_rimasti = self._tentativi_massimi
+        self._numero_segreto = None
 
     def reset(self):
-        # Questo metodo resetta il gioco in qualsiasi momento
-        self._segreto = random.randint(0, self._NMax)
-        self._T = self._TMax
-        print(self._segreto)
+        # questo metodo resetta il gioco per iniziare una nuova partita
+        self._numero_segreto = random.randint(0, self._numero_massimo)
+        self._tentativi_rimasti = self._tentativi_massimi
 
-    def play(self, guess):
+    def play(self, tentativo):
         """
-        Funzione che esegue uno step del gioco
-        :param guess: int
-        :return: 0 se vinto,
-        -1 se segreto è più piccolo,
-        1 se segreto è più grandde,
-        2 se ho finito le vite
+        gestisce le dinamiche del gioco
+        :param tentativo: int
+        :return: 0 se si ha vinto, -1 se il numero segreto è più piccolo del tentativo, 1 se il numero segreto è più
+        grande del tentativo, 2 se si ha perso (perché le "vite" sono finite)
         """
-        # da fuori ci arriva un tentativo, confrontiamo
-        # il tentatvo con il segreto
+        # questo metodo gestisce la dinamica di gioco confrontando il numero inserito dall'utente con il numero segreto
+        # scelto, randomicamente, dal server
+        self._tentativi_rimasti = self._tentativi_rimasti - 1
+        if tentativo == self._numero_segreto:
+            return 0 # lo zero indica la vittoria
+        if self._tentativi_rimasti == 0:
+            return 2 # il due indica che la partita è stata persa definitivamente in quanto sono terminate le "vite"
+        if tentativo > self._numero_segreto:
+            return -1 # il -1 indica che il numero segreto è più piccolo del tentativo inserito dall'utente
+        return 1
 
-        self._T -= 1
-
-        if guess == self._segreto:
-            return 0 # ho vinto!!
-
-        if self._T == 0:
-            return 2 # ho perso definitivamente
-
-        if guess > self._segreto:
-            return -1 # il segreto è più piccolo
-
-        return 1 #il segreto è più grande
-
-    @property
-    def NMax(self):
-        return self._NMax
-
-    @NMax.setter
-    def NMax(self, value):
-        self._NMax = value
-
-    @property
-    def TMax(self):
-        return self._TMax
-
-    @NMax.setter
-    def TMax(self, value):
-        self._TMax = value
-
-    @property
-    def T(self):
-        return self._T
-
-    @property
-    def segreto(self):
-        return self._segreto
 
 if __name__ == "__main__":
     m = Model()
     m.reset()
-    print(m.play(80))
+    print(m.play(50))
     print(m.play(10))
+
+# E' sempre una buona prassi testare il modello senza l'interfaccia grafica!
